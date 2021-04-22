2a937b11aef906fb6b70878b5dabfa9cb98ef836
Morrism1 <morrismontana0@gmail.com>
2021-04-22 08:00:28 +0200

    Create page three

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,41 @@ import Navbar from './Navbar';
 import GenericPage from './GenericPage';
 import Button from './Button';
 import { pageTwoH2, pageTwoDownpart } from './PageTwo';
-
+import { pageThreeH2, pageThreeDownpart } from './PageThree';
 
 function App() {
-  const h2 = (<><p>We do software testing</p><p>for flawless products.</p><p>With a purpose.</p></>);
+  const h2 = (
+    <>
+      <p>We do software testing</p>
+      <p>for flawless products.</p>
+      <p>With a purpose.</p>
+    </>
+  );
 
   return (
     <>
       <Navbar />
-      <GenericPage header={h2} body={<Button text={'Get in touch'} />} bg={'bg-primary'} />
-      <GenericPage header={pageTwoH2} body={pageTwoDownpart} bg={'bg-white'} />
-
+      <div>
+        <GenericPage
+          header={h2}
+          body={<Button text={'Get in touch'} />}
+          bg={'bg-primary'}
+        />
+      </div>
+      <div className="mb-12 page-two">
+        <GenericPage
+          header={pageTwoH2}
+          body={pageTwoDownpart}
+          bg={'bg-white'}
+        />
+      </div>
+      <div className="bg-primary page-three">
+        <GenericPage
+          header={pageThreeH2}
+          body={pageThreeDownpart}
+          bg={'bg-primary'}
+        />
+      </div>
     </>
   );
 }
