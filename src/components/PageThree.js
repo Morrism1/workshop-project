879ea4df086e684553cbe9a62bad36a24ea53dfa
Morrism1 <morrismontana0@gmail.com>
2021-04-22 07:59:43 +0200

    Add page three content

---
+++ src/components/PageThree.js
@@ -0,0 +1,5 @@
+const pageThreeH2 = "Seamless integration. Thorough testing. Better results."
+
+const pageThreeDownpart = (<div className=" text-blue py-16 absolute ml-80 mr-48 page-two-text p"><p>Our software testers are led and guided by a group of QA-industry professionals. This guarantees clear communication and a team with an in-depth understanding of your business and testing needs. After an initial assessment, we tailor our workflow to seamlessly integrate into your current processes. We can use your preferred tools and adapt our services based on your communication models, timeline, and more.</p></div>)
+
+export { pageThreeH2, pageThreeDownpart }
