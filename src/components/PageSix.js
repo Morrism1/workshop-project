7e3b3a0844f48e2f015cd8dbc1a64567eaae8585
Morrism1 <morrismontana0@gmail.com>
2021-04-22 08:55:02 +0200

    Create page six contents

---
+++ src/components/PageSix.js
@@ -0,0 +1,5 @@
+const pageSixH2 = "Performance testing"
+
+const pageSixDownpart = (<div className=" text-blue py-16 absolute ml-80 mr-48 page-two-text p"><p>Ensure customer satisfaction by guaranteeing optimal performance during standard and peak workloads. We identify key performance criteria, provide you with information about speed, stability and scalability, and determine which areas need improvement.</p></div>)
+
+export { pageSixH2, pageSixDownpart }
