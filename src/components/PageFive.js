5f110b6fdcbdfc49f6480bc1f60f347ce41dfa52
Morrism1 <morrismontana0@gmail.com>
2021-04-22 16:17:46 +0200

    Refactor page styles

--- src/components/PageFive.js
+++ src/components/PageFive.js
@@ -1,5 +1,5 @@
 const pageFiveH2 = 'Automated testing'
 
-const pageFiveDownContent = (<div className="text-blue py-16 absolute ml-80 mr-48 page-two-text p"><p className="mb-4">Save time and increase your testing coverage with automated testing.</p><p className="mb-4">We offer automated testing for web, mobile, and desktop apps.</p><ul className="list-disc ml-4"><li>API testing</li><li>UI testing</li><li>JavaScript, Python</li><li>Selenium, Cypress</li><li>Review of existing automation</li></ul><p></p></div>)
+const pageFiveDownContent = (<div className="text-blue py-16 absolute page-two-text pr-16"><p className="mb-4">Save time and increase your testing coverage with automated testing.</p><p className="mb-4">We offer automated testing for web, mobile, and desktop apps.</p><ul className="list-disc ml-4"><li>API testing</li><li>UI testing</li><li>JavaScript, Python</li><li>Selenium, Cypress</li><li>Review of existing automation</li></ul><p></p></div>)
 
 export { pageFiveH2, pageFiveDownContent }
